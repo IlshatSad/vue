62bb65d60c757083807593f48ad5ba394210958d
IlshatSad <ilshatsad0@gmail.com>
2020-02-17 23:36:54 +0300

    update hot-reload

--- webpack.config.develop.js
+++ webpack.config.develop.js
@@ -1,17 +1,21 @@
+const webpackCopy = require('copy-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 const outputDirectory = 'dist';
 
 module.exports = {
-    mode: 'production',
-    entry: ["./src/index.js"],
+    mode: 'development',
+    entry: ["webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000", "./src/index.js"],
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, outputDirectory),
         publicPath: '/',
-        globalObject: `(typeof self !== 'undefined' ? self : this)`
+        globalObject: `(typeof self !== 'undefined' ? self : this)`,
+        hotUpdateChunkFilename: ".hot/[id].[hash].hot-update.js",
+        hotUpdateMainFilename: ".hot/[hash].hot-update.json"
     },
     module: {
         rules: [
@@ -42,7 +46,13 @@ module.exports = {
         new VueLoaderPlugin(),
         new webpack.DefinePlugin({
             'typeof window': JSON.stringify('object')
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpackCopy([
+            { from: 'node_modules/vue/dist/vue.min.js', to: 'extLib/vue.min.js' },
+            { from: 'node_modules/vue-material/dist/vue-material.min.js', to: 'extLib/vue-material.min.js' },
+            { from: 'node_modules/vuex/dist/vuex.min.js', to: 'extLib/vuex.min.js' }
+          ])
     ],
     externals: {
         vue: 'Vue',
