94a47939197aef9f4b35350057ad1bb2b1068421
ilshat.sadretdinov <Ilshat.Sadretdinov@tatar.ru>
2020-02-18 22:07:29 +0300

    get Characters from api

--- src/__data__/api/api.js
+++ src/__data__/api/api.js
@@ -1,15 +1,26 @@
+import getHashes from './getHash';
 import axios from 'axios';
+
 const BASE_URL = require('../constants/urls').URLS;
 
 const Api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: BASE_URL.MARVEL_CHARACTERS,
   headers: {
     'Content-Type': 'application/json; charset=utf-8'
   }
 })
 
-export const request = {
-  getCharacters: () => {
-
+export default {
+  getCharacters: async () => {
+    let hash = await getHashes()
+    return Api.get('/v1/public/characters', {
+      params: {
+        limit: 10,
+        offset: 0,
+        ts: hash.date,
+        apikey: hash.apikey,
+        hash: hash.hash
+      }
+    })
   }
 }
