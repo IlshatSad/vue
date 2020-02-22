284bc1353fc3cce700c54ca862e8976378c1d537
IlshatSad <ilshatsad0@gmail.com>
2020-02-23 00:49:17 +0300

    add new pages

---
+++ src/pages/Character.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Character Page</h1>
+</template>
+
+<style lang="stylus" scoped>
+
+</style>
+
+<script>
+export default {
+  name: 'Character',
+  props: {
+    id: {
+      type: Number
+    }
+  },
+  created(){
+    console.log('this :', this);
+  },
+  computed: {
+
+  }
+}
+</script>
---
+++ src/pages/Favorites.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Favorites Page</h1>
+</template>
+
+<style lang="stylus" scoped>
+  
+</style>
+
+<script>
+import PersonalCard from '../components/personal-card.vue';
+export default {
+  name: 'Favorites',
+  created() {
+    console.log('Favorites');
+  }
+}
+</script>
